2da0461496909c6bebd97b35ca4cb477aee1b289
Godwin024 <simondavid5220@gmail.com>
2026-04-09 10:13:24 +0100

    littelloop

--- Littleloops/PrintNum.java
+++ Littleloops/PrintNum.java
@@ -1,1 +1,19 @@
+import java.util.Scanner;
 
+public class PrintNum{
+  
+public static void main(String[] args){
+  Scanner input = new Scanner(System.in);
+
+   System.out.print("Enter your N digit:");
+    
+    int userInput = input.nextInt();
+
+     for ( int count =1; count <= userInput; count++ ){
+     System.out.println(count);
+    
+    }
+
+  }
+
+}
