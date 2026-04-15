5177874408930c2c2aa2ff55b7c40fe34b8b0cb7
Godwin024 <simondavid5220@gmail.com>
2026-04-15 17:10:53 +0100

    1

---
+++ FireDrillTwo/TaskOne.java
@@ -0,0 +1,32 @@
+public class TaskOne{
+
+   public static void main(String[] args){
+
+       int sum = 0;
+     for(int count = 1; count <=10; count++){
+          
+          if(count <=10){
+          
+        sum += count;
+        System.out.println(sum);
+       
+
+}
+
+       
+
+
+
+}
+
+      
+
+
+
+
+}
+
+
+
+
+}
