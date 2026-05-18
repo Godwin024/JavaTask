3fee4d21bdf2ce981c4e5cec73c90365a1fefffe
Godwin024 <simondavid5220@gmail.com>
2026-05-18 08:07:08 +0100

    task

--- Wahala/OshoFreePromoEngine.java
+++ Wahala/OshoFreePromoEngine.java
@@ -3,49 +3,41 @@ public class OshoFreePromoEngine {
 
     public static void main (String[] args){
 
-
-
 }
 
-
-
-
-
     public static double calculatePercentDiscount(int cartTotal , String promoCode){
-        double price = 0;
-
-                String promo = promoCode.toUpperCase();
+        double mainPrice = 0;
+        String promo = promoCode.toUpperCase();
 
             if (cartTotal > -1) {
             if(cartTotal < 5000){
                 mainPrice = cartTotal;
-        }
+    }
 
-               else if (cartTotal >= 5000 && cartTotal <= 14999 && promo.equals("STARTER10")){
+            else if (cartTotal >= 5000 && cartTotal <= 14999 && promo.equals("STARTER10")){
                        double discount = cartTotal * 0.1;
                             mainPrice = cartTotal - discount;
+    }
 
-                }
-
-               else if (cartTotal >= 15000 && cartTotal <= 29999 && promo.equals("BIGBOY20")){
+            else if (cartTotal >= 15000 && cartTotal <= 29999 && promo.equals("BIGBOY20")){
                        double discount = cartTotal * 0.2;
                             mainPrice = cartTotal - discount;
 
-                }
+    }
 
-               else if (cartTotal >= 30000 && promo.equals("OSHOFREE35")){
+            else if (cartTotal >= 30000 && promo.equals("OSHOFREE35")){
                        double discount = cartTotal * 0.35;
                             mainPrice = cartTotal - discount;
-                }
+   }
 
-                        else{
+             else{
+
                             mainPrice = cartTotal;
-                        }
+                  }
+
 
                 }
                     
-
-
                 return mainPrice;
     }
 
